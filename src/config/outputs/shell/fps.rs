6b59c4f8b64b989056d10ebf7be8708fa1d2f9eb
Mateusz <dragonn@op.pl>
2022-01-17 21:12:25 +0100

    Implement FPS widget back, update smithay-egui (#12)
    * fps widget reimplementation
    
    * remove some render_output experiments
    
    * remove some rendering test
    
    * cargo update

--- src/config/outputs/shell/fps.rs
+++ src/config/outputs/shell/fps.rs
@@ -1,4 +1,6 @@
+use std::cell::RefCell;
 use std::rc::Rc;
+use std::time::Instant;
 
 use egui::Ui;
 use rhai::plugin::*;
@@ -9,18 +11,46 @@ use crate::output_manager::Output;
 use super::widget::*;
 
 #[derive(Debug, Clone)]
-pub struct Fps(Output);
+struct Inner {
+    old_fps: u32,
+    old_fps_timeout: Instant,
+}
+
+#[derive(Debug, Clone)]
+pub struct Fps {
+    output: Output,
+    inner: Rc<RefCell<Inner>>,
+}
 
 impl Fps {
     pub fn new(output: Output) -> Self {
-        Self(output)
+        let current_fps = output.get_fps();
+        Self {
+            output,
+            inner: Rc::new(RefCell::new(Inner {
+                old_fps: current_fps,
+                old_fps_timeout: Instant::now(),
+            })),
+        }
     }
 }
 
 impl Widget for Fps {
+    #[cfg(feature = "debug")]
     fn render(&self, ui: &mut Ui, _config_tx: &Sender<ConfigEvent>) {
-        // ui.text(format!("FPS: {}", self.0.get_fps() as u32));
-        ui.label(format!("FPS: {}", 0));
+        let mut inner = self.inner.borrow_mut();
+        if inner.old_fps_timeout.elapsed().as_secs() >= 1 {
+            inner.old_fps = self.output.get_fps();
+            ui.ctx().request_repaint();
+            inner.old_fps_timeout = Instant::now();
+        }
+        ui.label(format!("FPS: {}", inner.old_fps as u32));
+        //ui.label(format!("FPS: {}", 0));
+    }
+
+    #[cfg(not(feature = "debug"))]
+    fn render(&self, ui: &mut Ui, _config_tx: &Sender<ConfigEvent>) {
+        ui.label("FPS: debug feature not enabled");
     }
 }
 
--- src/state.rs
+++ src/state.rs
@@ -385,10 +385,17 @@ impl Anodium {
             // TODO:
             let _todo = pointer_image;
         }
-
-        self.workspace
+        let render_result = self
+            .workspace
             .render_output(renderer, output, 0, [0.1, 0.1, 0.1, 1.0], &elems)
             .unwrap();
+
+        if let Some(render_result) = render_result {
+            if !render_result.is_empty() {
+                #[cfg(feature = "debug")]
+                output.tick_fps();
+            }
+        }
 
         Ok(())
     }
